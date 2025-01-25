b51f1808df0e9f98f98ed5fd3cf32bccf69e9321
household-four <barringerchristen4@gmail.com>
2025-01-24 16:07:52 -0800

    initial app setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./app.component').then(m => m.AppComponent)
+    },
+    {
+        path: 'library',
+        loadComponent: () =>
+            import('./library/library.component').then(m => m.LibraryComponent)
+    },
+    {
+        path: 'lab',
+        loadComponent: () =>
+            import('./lab/lab.component').then(m => m.LabComponent)
+    }
+];
