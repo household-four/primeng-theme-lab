5f7f6990979856faa2f981619386d2f0e1279fd7
household-four <barringerchristen4@gmail.com>
2025-04-28 21:55:38 -0700

    added more primeng components

--- src/app/primeng-components/primeng/primeng.component.ts
+++ src/app/primeng-components/primeng/primeng.component.ts
@@ -22,6 +22,17 @@ import { SplitButtonModule } from 'primeng/splitbutton';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
 import { TooltipModule } from 'primeng/tooltip';
+import { PasswordModule } from 'primeng/password';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
+import { Select, SelectModule } from 'primeng/select';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SliderModule } from 'primeng/slider';
+import { TextareaModule } from 'primeng/textarea';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { ToggleSwitch, ToggleSwitchModule } from 'primeng/toggleswitch';
+import { TreeSelectModule } from 'primeng/treeselect';
+
 
 @Component({
   selector: 'app-primeng',
@@ -43,13 +54,23 @@ import { TooltipModule } from 'primeng/tooltip';
     KnobModule,
     ListboxModule,
     MultiSelectModule,
+    PasswordModule,
+    RadioButtonModule,
+    RatingModule,
     ReactiveFormsModule,
     ScrollPanelModule,
+    SelectModule,
+    SelectButtonModule,
+    SliderModule,
     SpeedDialModule,
     SplitButtonModule,
+    TextareaModule,
     ToastModule,
+    ToggleButtonModule,
+    ToggleSwitchModule,
     ToolbarModule,
-    TooltipModule
+    TooltipModule,
+    TreeSelectModule
   ],
   templateUrl: './primeng.component.html',
   styleUrl: './primeng.component.scss'
@@ -81,7 +102,91 @@ export class PrimengComponent {
   ];
 
   selectedCity = { name: 'Rome', code: 'RM' };
+  selectedCity2 = { name: 'Istanbul', code: 'IST' };
   selectedCities = [];
+
+  rating = 3;
+
+  stateOptions = [
+    { name: 'Option 1', code: '1' },
+    { name: 'Option 2', code: '2' },
+  ];
+  selectedState = { name: 'Option 2', code: '2' };
+
+  sliderValue=10;
+
+  checked1 = true;
+  checked2=true;
+
+  // todo make your own values for the treenodes instead of copying primeng
+  treeNodes = [
+    {
+      key: '0',
+      label: 'Documents',
+      data: 'Documents Folder',
+      icon: 'pi pi-fw pi-inbox',
+      children: [
+        {
+          key: '0-0',
+          label: 'Work',
+          data: 'Work Folder',
+          icon: 'pi pi-fw pi-cog',
+          children: [
+            { key: '0-0-0', label: 'Expenses.doc', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
+            { key: '0-0-1', label: 'Resume.doc', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
+          ]
+        },
+        {
+          key: '0-1',
+          label: 'Home',
+          data: 'Home Folder',
+          icon: 'pi pi-fw pi-home',
+          children: [{ key: '0-1-0', label: 'Invoices.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
+        }
+      ]
+    },
+    {
+      key: '1',
+      label: 'Events',
+      data: 'Events Folder',
+      icon: 'pi pi-fw pi-calendar',
+      children: [
+        { key: '1-0', label: 'Meeting', icon: 'pi pi-fw pi-calendar-plus', data: 'Meeting' },
+        { key: '1-1', label: 'Product Launch', icon: 'pi pi-fw pi-calendar-plus', data: 'Product Launch' },
+        { key: '1-2', label: 'Report Review', icon: 'pi pi-fw pi-calendar-plus', data: 'Report Review' }
+      ]
+    },
+    {
+      key: '2',
+      label: 'Movies',
+      data: 'Movies Folder',
+      icon: 'pi pi-fw pi-star-fill',
+      children: [
+        {
+          key: '2-0',
+          icon: 'pi pi-fw pi-star-fill',
+          label: 'Al Pacino',
+          data: 'Pacino Movies',
+          children: [
+            { key: '2-0-0', label: 'Scarface', icon: 'pi pi-fw pi-video', data: 'Scarface Movie' },
+            { key: '2-0-1', label: 'Serpico', icon: 'pi pi-fw pi-video', data: 'Serpico Movie' }
+          ]
+        },
+        {
+          key: '2-1',
+          label: 'Robert De Niro',
+          icon: 'pi pi-fw pi-star-fill',
+          data: 'De Niro Movies',
+          children: [
+            { key: '2-1-0', label: 'Goodfellas', icon: 'pi pi-fw pi-video', data: 'Goodfellas Movie' },
+            { key: '2-1-1', label: 'Untouchables', icon: 'pi pi-fw pi-video', data: 'Untouchables Movie' }
+          ]
+        }
+      ]
+    }
+  ];
+  selectedTreeNodes: any;
+
 
   // for toast displaying
   success() {
